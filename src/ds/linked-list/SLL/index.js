76f885efcc3a4dd1a9de333f203e4ae360d515b3
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-03-02 05:43:33 +0530

    feat: #LinkedList
    Added implementation for singly and doubly linkedlist using TDD>

---
+++ src/ds/linked-list/SLL/index.js
@@ -0,0 +1,90 @@
+class Node {
+  value = null;
+  next = null;
+
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+  toString() {
+    return `${this.value}`;
+  }
+}
+
+class LinkedList {
+  head = null;
+  tail = null;
+  length = 0;
+  constructor(value) {
+    const node = new Node(value);
+    this.head = node;
+    this.tail = node;
+    this.length++;
+  }
+
+  _traverseToIndex(index) {
+    let i = 0,
+      current = this.head;
+
+    while (i < index) {
+      current = current.next;
+      i++;
+    }
+    return current;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+  }
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+
+    const node = new Node(value);
+    let pointerNode = this._traverseToIndex(index - 1);
+
+    node.next = pointerNode.next;
+    pointerNode.next = node;
+
+    this.length++;
+  }
+  remove(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      this.length--;
+      return;
+    }
+    let pointerNode = this._traverseToIndex(index - 1),
+      unwantedNode = pointerNode.next;
+
+    pointerNode.next = unwantedNode.next;
+
+    this.length--;
+    return;
+  }
+  get(index) {
+    return this._traverseToIndex(index).value;
+  }
+  toString() {
+    let current = this.head;
+    let str = '';
+    while (current) {
+      str += `[${current.value} ${String(current.next)} ] --> `;
+      current = current.next;
+    }
+    return str;
+  }
+}
+
+export default LinkedList;
---
+++ src/ds/linked-list/SLL/index.test.js
@@ -0,0 +1,78 @@
+import LinkedList from '.';
+
+/**
+ * create
+ * append
+ * prepend
+ * get
+ * remove
+ *
+ */
+let list;
+beforeEach(() => {
+  list = new LinkedList(10);
+});
+
+describe('Testing Singly Linked List', () => {
+  it('creates a linked list with a single node', () => {
+    expect(list.length).toBe(1);
+    expect(list.head.value).toBe(10);
+    expect(list.tail.value).toBe(10);
+  });
+  it('appends a node to the linked list with a given value', () => {
+    list.append(20);
+    expect(list.length).toBe(2);
+    expect(list.head.value).toBe(10);
+    expect(list.tail.value).toBe(20);
+  });
+  it('gets a node at given index', () => {
+    list.append(20);
+    list.append(30);
+    list.append(40);
+    expect(list.get(2)).toBe(30);
+  });
+  it('prepends a node to the linked list with a given value', () => {
+    list.prepend(100);
+    expect(list.length).toBe(2);
+    expect(list.head.value).toBe(100);
+    expect(list.tail.value).toBe(10);
+  });
+  it('inserts a node at the beginning', () => {
+    list.append(20);
+    list.append(30);
+    list.append(50);
+    list.insert(0, 40);
+
+    expect(list.length).toBe(5);
+    expect(list.get(0)).toBe(40);
+  });
+
+  it('inserts a node at the end', () => {
+    list.append(20);
+    list.append(30);
+    list.append(50);
+    list.insert(3, 40);
+
+    expect(list.length).toBe(5);
+    expect(list.get(3)).toBe(40);
+    expect(list.get(4)).toBe(50);
+  });
+
+  it('inserts a node in the middle', () => {
+    list.append(20);
+    list.append(30);
+    list.append(50);
+    list.insert(2, 40);
+
+    expect(list.get(2)).toBe(40);
+    expect(list.length).toBe(5);
+  });
+  it('removes a node at specific index', () => {
+    list.append(20);
+    list.append(30);
+    list.append(50);
+    list.remove(2);
+    expect(list.length).toBe(3);
+    expect(list.get(2)).toBe(50);
+  });
+});
